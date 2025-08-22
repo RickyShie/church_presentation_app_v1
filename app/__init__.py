e5c3018de42a8fbd9e6ea9e4d2c6f00d151ac5a3
christine <rickyshie0724@gmail.com>
2025-08-22 14:25:05 +0900

    Modify __init__.py file after installing flask_socketio library.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+socketio = SocketIO(app)
 
 from app import routes
